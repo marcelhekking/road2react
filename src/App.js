ed45679ba71ceeb5131443b3bc8eb8922bbf072b
Marcel Hekking <m.hekking@isprojects.nl>
2019-04-17 17:18:51 +0200

    Chapter 4, part 1 (ui testing)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import PropTypes from "prop-types";
 import "./App.css";
 
 const DEFAULT_QUERY = "redux";
@@ -183,16 +184,29 @@ const smallColumn = {
   width: "10%"
 };
 
-class Button extends Component {
-  render() {
-    const { onClick, className = "", children } = this.props;
+const Button = ({ onClick, className = "", children }) => (
+  <button onClick={onClick} className={className} type="button">
+    {children}
+  </button>
+);
 
-    return (
-      <button onClick={onClick} className={className} type="button">
-        {children}
-      </button>
-    );
-  }
-}
+Button.defaultProps = {
+  className: ""
+};
+
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number
+    })
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired
+};
 
 export default App;
+
+export { Button, Search, Table };
