ebc3439039e67c43f0490ddb09c6b13d800a7dd4
Marcel Hekking <m.hekking@isprojects.nl>
2019-04-25 11:58:32 +0200

    Chapter 4, part 2(ui testing ...get it now, forgot a bit refactoring on `Table`)

--- src/App.js
+++ src/App.js
@@ -144,33 +144,28 @@ const Search = ({ value, onChange, onSubmit, children }) => (
   </form>
 );
 
-class Table extends Component {
-  render() {
-    const { list, onDismiss } = this.props;
-    return (
-      <div className="table">
-        {list.map(item => (
-          <div key={item.objectID} className="table-row">
-            <span style={largeColumn}>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span style={midColumn}>{item.author}</span>
-            <span style={smallColumn}>{item.num_comments}</span>
-            <span style={smallColumn}>{item.points}</span>
-            <span>
-              <Button
-                onClick={() => onDismiss(item.objectID)}
-                className="button-inline"
-              >
-                Dismiss
-              </Button>
-            </span>
-          </div>
-        ))}
+const Table = ({ list, onDismiss }) => (
+  <div className="table">
+    {list.map(item => (
+      <div key={item.objectID} className="table-row">
+        <span style={largeColumn}>
+          <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={midColumn}>{item.author}</span>
+        <span style={smallColumn}>{item.num_comments}</span>
+        <span style={smallColumn}>{item.points}</span>
+        <span style={smallColumn}>
+          <Button
+            onClick={() => onDismiss(item.objectID)}
+            className="button-inline"
+          >
+            Dismiss
+          </Button>
+        </span>
       </div>
-    );
-  }
-}
+    ))}
+  </div>
+);
 
 const largeColumn = {
   width: "40%"
